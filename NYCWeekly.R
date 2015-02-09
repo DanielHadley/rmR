d6cf607733267ac74bc73a535454a17754428267
DanielHadley <dphnrome@gmail.com>
2015-02-09 12:50:22 -0500

    fully functional .md in nyc.R: now on to Chicago

--- NYCWeekly.R
+++ NYCWeekly.R
@@ -6,9 +6,10 @@ library(ggmap)
 today <- Sys.Date()
 lastWeek <- today - 7
 
-todayText <- format(today, "%b %d")
-lastWeekText <- format(lastWeek, "%b %d")
+todayText <- format(today, "%B %d")
+lastWeekText <- format(lastWeek, "%B %d")
 Year <- format(today, "%Y")
+todayUSA <- format(today, '%m-%d-%Y')
 
 ## Plain Text:
 # https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=descriptor='Rat Sighting'AND created_date > '2015-01-20'
@@ -23,26 +24,32 @@ d <- nyc
 # Dot map 
 map.center <- geocode("New York City, NY")
 SHmap <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom = 11, color='bw')
-SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 2, alpha = .7, bins = 26, color="red",) + 
-  ggtitle(paste("Rat Calls: ", lastWeekText, " to ", todayText, ", ", Year, sep="")) 
+SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 2, alpha = .7, bins = 26, color="red",) 
 
 ggsave(paste("/Users/dphnrome/Google Drive/RatMaps/posts/NYC_Rat_Map_",today,".png",sep=""), dpi=200, width=4, height=4)
 
 
+# total calls
+totalCalls <- nrow(d)
 
-# Start writing to an output file
-sink(paste("/Users/dphnrome/Documents/Git/ratmaps/_posts/test-", today,".md",sep=""))
+#### Start writing to an output file ####
+# This makes the .md blog posts
+sink(paste("/Users/dphnrome/Documents/Git/ratmaps/_posts/", today,"-NYC-Rats.md",sep=""))
 
 cat("---\n")
 cat("layout: post\n")
-cat(sprintf('title: NYC Weekly %s\n', today))
+cat(sprintf('title: NYC Weekly %s\n', todayUSA))
 cat("tags: NYC weekly\n")
 cat("---\n")
 
+cat("\n")
 
+cat(sprintf("Between %s and %s, there were %s calls to New York City's 311 line about rats.\n", lastWeekText, todayText, totalCalls)) 
 
+cat(sprintf("![_config.yml](http://googledrive.com/host/0BxOPuM_gK7bqUW85bjZUd1UwTGs/posts/NYC_Rat_Map_%s.png)\n", today))
 
-cat("![_config.yml](http://googledrive.com/host/0BxOPuM_gK7bqUW85bjZUd1UwTGs/posts/NYC_Rat_Map_",today,".png\n")
+cat("\n")
+cat("Each red dot represents one call about a rat seen in that location. The dots are transparent, so multiple calls from one location will be seen as a solid dot. The usual caveats apply: more calls do not necessarily mean that there are more rats in that location than other parts of the city. A cluster of dots could indicate that one resident is unusually vocal about rodents in his or her neighborhood. Also, more densely populated neighborhoods tend to make more calls to 311.")
 
 # Stop writing to the file
 sink()
--- NYCWeekly.sh
+++ NYCWeekly.sh
@@ -3,32 +3,16 @@
 #### Created by Daniel Hadley #####
 # Automates the creation and posting of new rat maps
 
-# run the script that creates the newest map
-Rscript /Users/dphnrome/Documents/Git/rmR/NYCWeekly.R
-
-
-## Takes last week's post, copies it, and replaces map
-today=`date +%Y-%m-%d` # date as variable
-lastWeek=$(date -j -v-7d +"%Y-%m-%d")
-
-UStoday=`date +%m-%d-%Y` # US date as variable
-USlastWeek=$(date -j -v-7d +"%m-%d-%Y")
-
 ### Pull the latest version in case changes were made
 
 cd /Users/dphnrome/Documents/Git/ratmaps/
 git pull
 
+# run the script that creates the newest map and .md file
+Rscript /Users/dphnrome/Documents/Git/rmR/NYCWeekly.R
+
+
 cd ./_posts/
-
-# Create a new weekly post as a copy of last week's
-cp $lastWeek-NYC-Rats.md ./$today-NYC-Rats.md
-
-# Replace the old date in the link to the map
-perl -pi -e "s/$lastWeek/$today/g" ./$today-NYC-Rats.md
-
-# Replace the old date in the link to the title
-perl -pi -e "s/$USlastWeek/$UStoday/g" ./$today-NYC-Rats.md
 
 
 # Commit all changes to the blog
