edbf17960d560f90b60928cd5d4be27c72a9be81
DanielHadley <dphnrome@gmail.com>
2015-02-11 17:13:03 -0500

    borough maps

--- oneOffScripts/NYC.R
+++ oneOffScripts/NYC.R
@@ -43,11 +43,51 @@ remove(allDays, days)
 ts$id <- seq(1, nrow(ts))
 
 
-# Dot map 
+#### Dot maps #### 
 map.center <- geocode("New York City, NY")
 SHmap <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom = 11, color='bw')
-SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 2, alpha = .7, bins = 26, color="red",) 
+SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = .5, alpha = .2, bins = 26, color="red",) 
 
-ggsave(paste("/Users/dphnrome/Google Drive/RatMaps/posts/NYC_Rat_Map_",today,".png",sep=""), dpi=200, width=4, height=4)
+ggsave(paste("../plots/NYC_Rat_Map_all.png",sep=""), dpi=200, width=4, height=4)
 
 
+map.center <- geocode("Brooklyn, NY")
+SHmap <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom = 13, color='bw')
+SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 1, alpha = .3, bins = 26, color="red",) 
+
+ggsave(paste("../plots/NYC_Rat_Map_Brooklyn.png",sep=""), dpi=200, width=4, height=4)
+
+
+map.center <- geocode("Queens, NY")
+SHmap <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom = 13, color='bw')
+SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 1, alpha = .3, bins = 26, color="red",) 
+
+ggsave(paste("../plots/NYC_Rat_Map_Queens.png",sep=""), dpi=200, width=4, height=4)
+
+
+map.center <- geocode("Bronx, NY")
+SHmap <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom = 13, color='bw')
+SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 1, alpha = .3, bins = 26, color="red",) 
+
+ggsave(paste("../plots/NYC_Rat_Map_Bronx.png",sep=""), dpi=200, width=4, height=4)
+
+
+map.center <- geocode("Staten Island, NY")
+SHmap <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom = 13, color='bw')
+SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 1, alpha = .3, bins = 26, color="red",) 
+
+ggsave(paste("../plots/NYC_Rat_Map_Staten_Island.png",sep=""), dpi=200, width=4, height=4)
+
+
+map.center <- geocode("Uptown Manhattan, NY")
+SHmap <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom = 13, color='bw')
+SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 1, alpha = .3, bins = 26, color="red",) 
+
+ggsave(paste("../plots/NYC_Rat_Map_Uptown_Manhattan.png",sep=""), dpi=200, width=4, height=4)
+
+
+map.center <- geocode("Downtown Manhattan, NY")
+SHmap <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom = 13, color='bw')
+SHmap + geom_point(data=d, aes(y=Latitude, x=Longitude), size = 1, alpha = .3, bins = 26, color="red",) 
+
+ggsave(paste("../plots/NYC_Rat_Map_Downtown_Manhattan.png",sep=""), dpi=200, width=4, height=4)
