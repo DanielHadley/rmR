9c1fcf919d0d68832fad59890cf7d1f8f3427cd9
DanielHadley <dphnrome@yahoo.com>
2015-03-21 22:30:39 +0000

    auto blog

--- ratBlogScripts/RatBlog.py
+++ ratBlogScripts/RatBlog.py
@@ -1,9 +1,12 @@
 import feedparser
 import newspaper
 
-d = feedparser.parse('https://news.google.com/news/feeds?output=rss&q=rats+rodents')
+d = feedparser.parse(
+'https://news.google.com/news/feeds?output=rss&q=rats+rodents')
 longLink = d.entries[0]['link'] 
-link_url = longLink[:longLink.rfind('url=')]
+link_url = longLink.split('url=', 1)[1]
+news_agency = link_url.split('.', 1)[1]
+news_agency = news_agency.split('/', 1)[0]
 
 from newspaper import Article
 
@@ -12,3 +15,26 @@ first_article.download()
 first_article.parse()
 first_article.nlp()
 print(first_article.summary)
+blog_title = first_article.title.encode('utf-8')
+blog_summary = first_article.summary.encode('utf-8')
+first_paragraph = first_article.text.encode('utf-8')
+sentences = 4
+seperate_sentences = first_paragraph.split('.')
+first_paragraph = '.'.join(seperate_sentences[:sentences])
+image = first_article.top_image.encode('utf-8')
+
+f = open('myfile.md','w')
+f.write('---\n')
+f.write('layout: post\n')
+f.write('title: ' + blog_title + '\n' )
+f.write('---\n')
+
+f.write('\n')
+f.write('![image of rat from news source](' + image + ')\n')
+f.write('\n')
+f.write('According to ' + news_agency + ': \n' )
+f.write('> ' + first_paragraph + '\n')
+f.write('(...Read Article)[' + link_url + ']\n')
+
+f.close() 
+
