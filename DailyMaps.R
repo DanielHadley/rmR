3fbcd7e35edafbc4ca9381546be9f0fbbbe533f5
DanielHadley <dphnrome@yahoo.com>
2015-03-25 00:25:02 +0000

    small fixes

--- DailyMaps.R
+++ DailyMaps.R
@@ -111,6 +111,7 @@ cat("---\n")
 cat("\n")
 
 cat("+[Chicago](#chicago)\n")
+cat("\n")
 cat("+[New York City](#nyc)\n")
 cat("+[Boston](#boston)\n")
 cat("\n")
@@ -128,7 +129,7 @@ cat("\n")
 cat("### Weekly\n")
 cat(sprintf("Between %s and %s, there were %s calls to Chicago's 311 line about rats. The average number of weekly calls for this time of year is %s.\n", lastWeekText, yesterdayText, chiLastWeek, chiAverageForThisTime)) 
 
-cat("![Chicago rat calls to 311 weekly map]({{ site.cityimages }}/daily/Rat_Map_Weekly.png)\n")
+cat("![Chicago rat calls to 311 weekly map]({{ site.cityimages }}/daily/Chicago_Rat_Map_Weekly.png)\n")
 
 cat("\n")
 cat("Each red dot represents one call about a rat seen in that location. The dots are transparent, so multiple calls from one location will be seen as a solid dot. The usual caveats apply: more calls do not necessarily mean that there are more rats in that location than other parts of the city. A cluster of dots could indicate that one resident is unusually vocal about rodents in his or her neighborhood. Also, more densely populated neighborhoods tend to make more calls to 311.")
